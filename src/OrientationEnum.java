a7d77436646df75c606ba43f01903f052dcd53e8
Unknown <diallomamadoury98@gmail.com>
2019-01-10 14:51:55 +0100

    enum orientation

---
+++ src/OrientationEnum.java
@@ -0,0 +1,19 @@
+
+public enum OrientationEnum {
+	 W("Ouest"),
+	 E("Est"),
+	 N("Nord"),
+	 S("Sud");
+	private String direction;
+	OrientationEnum(String orientation)	
+	{
+		setOrientation(orientation);
+	}
+	public String getOrientation() {
+		return direction;
+	}
+	public void setOrientation(String direction) {
+		this.direction = direction;
+	}
+
+}
